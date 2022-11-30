9917c537a022a3ab5af56bca98e1763407f0a271
kyle_karotko <kkrtk7@duck.com>
2022-11-30 01:39:38 -0500

    added queryURL var to js

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,3 @@
 var APIkey = "00a2ab0e16fa5ac0d1cd1818e2d4d52c";
-var city = "Nashville"
+var city = "Nashville";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q={city}&appid={APIkey}";
