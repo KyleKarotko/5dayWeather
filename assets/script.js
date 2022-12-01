9357cdf17e9a09b438f9972e4c7d3b99b000487e
kyle_karotko <kkrtk7@duck.com>
2022-12-01 17:36:06 -0500

    started citySearch Function

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,34 @@
+var searchFormEl = document.querySelector('#search-form');
 var APIkey = "00a2ab0e16fa5ac0d1cd1818e2d4d52c";
 var city = "Nashville";
+var lat ="";
+var lon ="";
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q={city}&appid={APIkey}";
+var APIurl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + APIkey;
+
+// Function to search for city
+function citySearch(event) {
+    event.preventDefault();
+    var searchInput = document.querySelector('#search-input').value;  
+    if (!searchInput) {
+      console.error('No city entered!');
+      return;
+    }
+  
+  
+  }
+  
+  searchFormEl.addEventListener('submit', citySearch);
+
+
+
+
+function init(){
+
+};
+
+
+
+
+init();
+
