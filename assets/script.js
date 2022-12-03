22f2c8939b7a52edfc524a86ce0d5bb8bad12e26
kyle_karotko <kkrtk7@duck.com>
2022-12-03 18:26:36 -0500

    updated vars/added current weather to card

--- assets/script.js
+++ assets/script.js
@@ -18,7 +18,8 @@ function citySearch(event) {
   
 //Function to get Cord from city name
 function convertCord (city){
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city +"&limit=5&appid=" + APIkey).then(function(response){
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city +"&limit=5&appid=" + APIkey)
+    .then(function(response){
     return response.json()
     }).then(function(data){
       console.log(data);
@@ -31,17 +32,40 @@ function convertCord (city){
 
 // function to get weather for city searched
   function getCurrentWeather(lat,lon,name){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${APIkey}`).then(function(response){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${APIkey}`)
+    .then(function(response){
       return response.json()
       }).then(function(data){
         console.log(data);
         console.log(name);
-        var weatherCard= document.createElement("div")
+        var temp = data.main.temp;
+        var humidity = data.main.humidity
+        var wind = data.wind.speed
+        var icon = data.weather[0].icon
+        var weatherCard= document.createElement("div");
         weatherCard.setAttribute("class","card");
         var cardTitle = document.createElement("h3");
         cardTitle.setAttribute("class","card-title");
         cardTitle.textContent = name;
         document.querySelector("#current-box").appendChild(weatherCard.appendChild(cardTitle));
+        var currentIcon = document.createElement('div')
+        currentIcon.setAttribute('class', 'card-body');
+        currentIcon.textContent = icon;
+        document.querySelector("#current-box").appendChild(weatherCard.appendChild(currentIcon));
+        var currentTemp = document.createElement('div');
+        currentTemp.setAttribute('class', 'card-body');
+        currentTemp.textContent = "Temperature: " + temp;
+        document.querySelector("#current-box").appendChild(weatherCard.appendChild(currentTemp));
+        var currentHumidity = document.createElement('div');
+        currentHumidity.setAttribute('class', 'card-body');
+        currentHumidity.textContent = "Humidity: " + humidity;
+        document.querySelector("#current-box").appendChild(weatherCard.appendChild(currentHumidity));
+        var currentWind= document.createElement('div');
+        currentWind.setAttribute('class', 'card-body');
+        currentWind.textContent = "Wind Speed: " + wind + "MPH";
+        document.querySelector("#current-box").appendChild(weatherCard.appendChild(currentWind));
+
+       
       });
   }
 
